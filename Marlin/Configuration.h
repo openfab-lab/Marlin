e4ba9bfc5d994f40bc448cd26fc6cefd848888e7
Louis Borsu <sat@satprod.net>
2018-10-03 14:59:36 +0200

    feat(update): correct bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -878,15 +878,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
+#define X_BED_SIZE 210
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -33
+#define X_MIN_POS -10
 #define Y_MIN_POS -10
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + X_MIN_POS
+#define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS
 #define Z_MAX_POS 240
 
 /**
